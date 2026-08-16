9f3f1ed28b1201eb3c434b415246e5cc19ab00f0
22-2 <17890m@protonmail.ch>
2026-08-16 19:31:00 +0900

    fix: Update target directory for Obsidian plugin copy in esbuild config

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -14,7 +14,7 @@ const buildOptions = {
   logLevel: "info",
   plugins: [
     obsidianCopyEsbuild({
-      targetDir: "E:\\AppData\\obsidian\\vaults\\suizen\\plugins\\japanese-manuscript-counter",
+      targetDir: "E:\\AppData\\obsidian\\vaults\\suizen\\.obsidian\\plugins\\japanese-manuscript-counter",
       force: true,
     }),
   ],
